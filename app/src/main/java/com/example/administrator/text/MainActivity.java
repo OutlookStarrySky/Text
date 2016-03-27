a841cc87c3900daca9aee6473d65d1716fda8a24
OutlookStarrySky <1197592676@qq.com>
2016-03-27 21:37:17 +0800

    添加注释

--- app/src/main/java/com/example/administrator/text/MainActivity.java
+++ app/src/main/java/com/example/administrator/text/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //隐藏title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
